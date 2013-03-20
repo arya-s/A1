e41ae57e08d73ba1128d4547f9fe7acf210622f4
arya-s <arya.darc@gmail.com>
2013-03-21 02:13:26 +0900

    Added simple player object and keyboard (arrows) handling.

--- js/A1main.js
+++ js/A1main.js
@@ -1,28 +1,55 @@
-// shim layer with setTimeout fallback
-//@see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function() {
-    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
-})();
+//Make sure jQuery is loaded else we can't use the hotkey plugin
+$(document).ready(function() {
+	// shim layer with setTimeout fallback
+	//@see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	window.requestAnimFrame = (function() {
+		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+	})();
 
-//Actual game loop
-(function gameloop() {
-    requestAnimFrame(gameloop);
-    update();
-    draw();
-})();
+	var last = new Date();
 
-function draw() {
-	g_eContext.clearRect(0, 0, 100, 100);
-	g_eContext.fillStyle = 'yellow';
-	g_eContext.moveTo(0, 100 / 2);
-	g_eContext.lineTo(100 / 2, 0);
-	g_eContext.lineTo(100, 100 / 2);
-	g_eContext.lineTo(100 / 2, 100);
-	g_eContext.lineTo(0, 100 / 2);
-	g_eContext.fill();
-}
+	function gameloop() {
+		var dt = new Date() - last;
 
-function update(){
-}
+		update(dt);
+		draw();
+
+		requestAnimFrame(gameloop);
+	}
+
+	//Start the game loop
+	gameloop();
+
+	function draw() {
+		g_eContext.clearRect(0, 0, g_eViewport.w, g_eViewport.h);
+		g_oPlayer.draw();
+	}
+
+	function update(dt) {
+		if (keydown.space) {
+			g_oPlayer.shoot();
+		}
+
+		if (keydown.left) {
+			g_oPlayer.x -= 5;
+		}
+
+		if (keydown.right) {
+			g_oPlayer.x += 5;
+		}
+
+		if (keydown.up) {
+			g_oPlayer.y += 5;
+		}
+
+		if (keydown.down) {
+			g_oPlayer.y -= 5;
+		}
+
+		//Clamp so we don't move the character out of the screen
+		g_oPlayer.x = g_oPlayer.x.clamp(0, g_eViewport.w - g_oPlayer.w);
+		g_oPlayer.y = g_oPlayer.y.clamp(0, g_eViewport.h - g_oPlayer.h);
+	}
+});
--- js/A1util.js
+++ js/A1util.js
@@ -1,29 +1,50 @@
-var g_sCanvasName = "A1MainCanvas";
-var g_eContext;
+var g_sCanvasName = "A1MainCanvas"
+, g_eContext
+, g_eViewport = {
+        w: window.innerWidth,
+        h: window.innerHeight
+}
+, g_oPlayer = null;
 
 function init(){
     var _widthRatio = 0.6
-    , _viewPort = {
-        w: window.innerWidth,
-        h: window.innerHeight
-    }
     , _canvas = {
         elem: document.getElementById(g_sCanvasName),
-        w: _viewPort.w * _widthRatio,
-        h: (_viewPort.w * _widthRatio) / 2
+        w: g_eViewport.w * _widthRatio,
+        h: (g_eViewport.w * _widthRatio) / 2
     };
 
     //Center the _canvas and scale it
     _canvas.elem.style.position = "fixed";
-    _canvas.elem.style.top = (_viewPort.h - _canvas.h) / 2 + "px";
-    _canvas.elem.style.left = (_viewPort.w - _canvas.w) / 2 + "px";
+    _canvas.elem.style.top = (g_eViewport.h - _canvas.h) / 2 + "px";
+    _canvas.elem.style.left = (g_eViewport.w - _canvas.w) / 2 + "px";
     _canvas.elem.setAttribute("width", _canvas.w);
     _canvas.elem.setAttribute("height", _canvas.h);
-    _canvas.elem.style.top = (_viewPort.h - _canvas.h) / 2;
-    _canvas.elem.style.left = (_viewPort.w - _canvas.w) / 2;
+    _canvas.elem.style.top = (g_eViewport.h - _canvas.h) / 2;
+    _canvas.elem.style.left = (g_eViewport.w - _canvas.w) / 2;
 
     g_eContext = _canvas.elem.getContext("2d");
+
+    //Create the player
+    g_oPlayer = {
+        color: "#00A",
+        x: 270,
+        y: 270,
+        w: 32,
+        h: 32,
+        draw: function(){
+            g_eContext.fillStyle = this.color;
+            g_eContext.fillRect(this.x, this.y, this.w, this.h);
+        },
+        shoot: function(){
+            console.log("Shootin'");
+        }
+    };
 }
 
 init();
 window.onresize = init;
+
+Number.prototype.clamp = function(min, max) {
+  return Math.min(Math.max(this, min), max);
+};
