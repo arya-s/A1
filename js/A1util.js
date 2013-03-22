2ea670b432d919433c288e148a0f63d7b71a9cca
arya-s <arya.darc@gmail.com>
2013-03-23 05:26:47 +0900

    Removed evil sleep function that shouldn't have been there.

--- js/A1util.js
+++ js/A1util.js
@@ -46,12 +46,3 @@ function A1Node(x, y){
 	this.x = x;
 	this.y = y;
 }
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
